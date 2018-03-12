9ffe6ecd2d33576f0604dbd9f34b5c165f0e5118
Dillon Beliveau <dillonbeliveau@gmail.com>
2018-03-12 23:51:31 +0000

    Json and standalone app

--- server.py
+++ server.py
@@ -2,9 +2,13 @@
 
 from flask import Flask
 import tiltaccess
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return str(tiltaccess.get_data())
+    return json.dumps(tiltaccess.get_data())
+
+if __name__ == "__main__":
+    app.run()
